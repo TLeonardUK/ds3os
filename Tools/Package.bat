28913c453991411d6fb26e7ecbdd10c7fc72e7f9
Tim Leonard <tim@programmingbytim.com>
2021-08-21 22:03:36 +0100

    CI Work

--- Tools/Package.bat
+++ Tools/Package.bat
@@ -1,8 +1,8 @@
 :: Assumed to be run from root directory.
 
-mkdir Publish
-mkdir Publish\Loader
-mkdir Publish\Server
-copy Resources\ReadMe.txt Publish\ReadMe.txt
-xcopy /s Bin\Server Publish\Server
-xcopy /s Bin\Loader\Package Publish\Loader
+mkdir DS3OS
+mkdir DS3OS\Loader
+mkdir DS3OS\Server
+copy Resources\ReadMe.txt DS3OS\ReadMe.txt
+xcopy /s Bin\Server DS3OS\Server
+xcopy /s Bin\Loader\Package DS3OS\Loader
