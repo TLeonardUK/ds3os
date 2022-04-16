d3691cd9c3e05710f3e5ffa2846f0dd3f51dd9af
Tim Leonard <tim@programmingbytim.com>
2022-04-16 14:58:28 +0100

    Get CI working with cmake.

--- Tools/generate_package.bat
+++ Tools/generate_package.bat
@@ -6,5 +6,11 @@ mkdir DS3OS\Server
 mkdir DS3OS\Prerequisites
 copy Resources\ReadMe.txt DS3OS\ReadMe.txt
 xcopy /s Resources\Prerequisites DS3OS\Prerequisites
-xcopy /s Bin\Server DS3OS\Server
-xcopy /s Bin\Loader\Package DS3OS\Loader
+
+xcopy /s Bin\x64_release\steam_appid.txt DS3OS\Server\steam_appid.txt
+xcopy /s Bin\x64_release\steam_api64.dll DS3OS\Server\steam_api64.dll
+xcopy /s Bin\x64_release\WebUI DS3OS\Server\WebUI
+xcopy /s Bin\x64_release\Server.exe DS3OS\Server\Server.exe
+xcopy /s Bin\x64_release\Server.pdb DS3OS\Server\Server.pdb
+
+xcopy /s Bin\x64_release\Loader\Package DS3OS\Loader
